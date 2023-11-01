a324ffbf6ab74e65cfce7a7e239a8f804b870e07
369webforall <369webforall@gmail.com>
2023-11-01 08:56:53 +0545

    Initial project setup

---
+++ app/layout.tsx
@@ -0,0 +1,33 @@
+import type { Metadata } from 'next';
+import { Inter as FontSans } from 'next/font/google';
+import './globals.css';
+import { cn } from '@/lib/utils';
+
+export const fontSans = FontSans({
+  subsets: ['latin'],
+  variable: '--font-sans',
+});
+
+export const metadata: Metadata = {
+  title: 'My Portfolio',
+  description: 'Javascript React Nextjs Tailwind css',
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en" suppressHydrationWarning>
+      <body
+        className={cn(
+          'min-h-screen bg-background font-sans antialiased',
+          fontSans.variable
+        )}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+import Image from 'next/image';
+import { Button } from '@/components/ui/button';
+export default function Home() {
+  return (
+    <main className="grid place-content-center h-screen">
+      <h1>home page</h1>
+    </main>
+  );
+}
